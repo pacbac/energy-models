09a0eaadfecabcd374ec3db5e397bb10ec917c7b
clayton <23279139+pacbac@users.noreply.github.com>
2018-02-04 23:46:41 -0800

    Practice with openpyxl for the competition

---
+++ Practice/script.py
@@ -0,0 +1,17 @@
+from openpyxl import load_workbook
+
+def main():
+    wb = load_workbook("C:/Python36/data.xlsx")
+    sheet = wb.active
+    table = {}
+    for c in range(4, sheet.max_column):
+        name = sheet.cell(row=1, column=c).value
+        table[name] = {}
+        for r in range(1, sheet.max_row):
+            objProp = sheet.cell(row=r, column=c).value
+            table[name][objProp] = sheet.cell(row=r, column=c).value
+    print(table["Amridge University"]["CITY"])
+
+
+if __name__ == "__main__":
+    main()
